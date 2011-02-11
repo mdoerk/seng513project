425c9ec01b088da0d8f494577670b07160f10d57
Cody <cody.cg@gmail.com>
2011-02-11 13:33:21 -0700

    change redirectTo() in signInController.js

--- app/controllers/signInController.js
+++ app/controllers/signInController.js
@@ -7,7 +7,7 @@ index = fuction(){
 	var user = user.authenticate(requst.username, request.password, function (authenticated){
 		
 		if(authenticated){
-			body = index;
+			redirectTo(index);
 		}else{
 			error = 'Sign in failed.';
 			body = 'signIn';

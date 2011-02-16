b718113d776b78db31cf0383836c7823d012bc7b
jewong <jewong0@gmail.com>
2011-02-15 20:50:46 -0700

    Restructured directory layout. Removed addPattern() method from router and include its functionality in add() method. Minor corrections and cleanup

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 var fs = require('fs'), 
 	http = require('http'),
-	path = require('path'),
 	url = require('url'),
 	util = require('util'), 
-	routes = require('./routes'); 
+	router = require('./lib/routes').router; 
 
 var port = 8124; 
 
 // Process command line arguments 
-// ARGV[0] always equals node
-// ARGV[1] always equals server.js
-var argv = process.ARGV.slice(2);
-for (var i = 0; i < argv.length; i++)
-{
+var argv = process.argv.slice(2);
+for (var i = 0; i < argv.length; i++) {
 	switch (argv[i]) {
 		case '--port':
 			port = parseInt(argv[i + 1], 10); 
@@ -21,10 +17,12 @@ for (var i = 0; i < argv.length; i++)
 			break; 
 	}
 }
- 
-var server = http.createServer(function(req, res) { 
+
+var server = function(req, res) {
 	util.log('Received ' + req.method + ' request for ' + req.url); 
 	var parsedUrl = url.parse(req.url, true);
-	routes.router.handle(parsedUrl.pathname, req, res); 
-}).listen(port); 
+	router.handle(parsedUrl.pathname, req, res); 
+}
+
+http.createServer(server).listen(port); 
 util.log('Server running at http://127.0.0.1:' + port);

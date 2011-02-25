24f372d424a27ea2f753e37d53f3c15f442472d0
Eric Kryski <e.kryski@gmail.com>
2011-02-25 14:12:39 -0700

    Added the nodeunit unit testing framework.  Changed the folder 'modules' to 'node_modules' so that there is easy module referencing.  Because of this I fixed the require path to 'testModule.js' in 'lib/routes.js'.  Also, added the testing framework info to the README file.  Added a 'test' folder with 'unitTests' and 'funcTests' sub directories with a couple unit tests.  Finally,  I added a script called 'runTests.js' to run the tests.

---
+++ test/unitTests/test-foo.js
@@ -0,0 +1,25 @@
+//Include your modules that you will need
+var testCase = require('nodeunit').testCase;
+
+module.exports = testCase({
+    setUp: function (callback) {
+        // Use this function to setup any thing you might 
+        // need to test (ie. connections to a test db).
+
+        this.foo = 'bar';
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+    yourTestName1: function (test) {
+        test.equals(this.foo, 'bar');
+        test.done();
+    },
+    yourTestName2: function (test) {
+        test.equals('bar'.length, 3);
+        test.done();
+    }
+    // More tests can follow. but don't forget the ','
+});

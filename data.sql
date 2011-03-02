2611d55b86edb891f88ad37256d78bbd97aabd17
mdoerk <mdoerk@ucalgary.ca>
2011-03-01 23:01:15 -0700

    added insert for example user

--- data.sql
+++ data.sql
@@ -1,1 +1,5 @@
-INSERT INTO issues(user_id, creation_date, status, title, description, link, location) VALUES (1, '10/02/11', 'open', 'First Issue', 'Here the first issue for tests', 'http://google.ca', 'calgary');
+INSERT INTO issues(user_id, created, status, title, description, link, location)
+VALUES (0, '10/02/11', 'open', 'First Issue', 'Here the first issue for tests', 'http://google.ca', 'calgary');
+
+INSERT INTO users(name, email, password)
+VALUES ('john', 'john@example.com', 'foo');

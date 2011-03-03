52996639431016f268486a4e97bf50785f824b35
Cody <cody.cg@gmail.com>
2011-03-03 14:02:49 -0700

    updated fixtures to users md5 hashs

--- db/fixtures/users.sql
+++ db/fixtures/users.sql
@@ -4,5 +4,4 @@
 
 
 /*	name:'one' email:'one' password:'two' */
-INSERT INTO users (name, email, password) VALUES ('one', 'one', 'b8572e307fcb2694778271edc56638cf7ee68969');
-/* Note: this password hash no longer works, it was uses with sha1. we're now using md5 hash. */
+INSERT INTO users (name, email, password) VALUES ('one', 'one', 'b8a9f715dbb64fd5c56e7783c6820a61');

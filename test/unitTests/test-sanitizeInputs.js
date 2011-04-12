cd8c6d0acf3dd2c7e788ed02367c9ca3cd9a2a10
ntaha <nasrullahs@gmail.com>
2011-04-11 22:44:50 -0600

    Added removeTags function in sanitizeInputs.js and added unit tests for it. Closes GH-296.

---
+++ test/unitTests/test-sanitizeInputs.js
@@ -0,0 +1,48 @@
+/*
+ * Tests for sanitizeInputs.js
+ * Created by: Nasrullah Taha
+ */
+ 
+var sanitizeInputs = require('sanitizeInputs.js');
+var nodeunit = require('nodeunit/nodeunit');
+
+module.exports = nodeunit.testCase({
+	setUp: function(callback) {
+		callback();
+	},
+	 
+	tearDown: function(callback) {
+		callback();
+	},
+
+	testSimpleString:function(test) {
+		var testString = "hello world!";
+		var result = sanitizeInputs.removeTags(testString);
+		test.deepEqual(result, testString);
+		test.done();
+	},
+	
+	testStringWithOneTag:function(test) {
+		var testString = "<a>hello world!";
+		var result = sanitizeInputs.removeTags(testString);
+		var expected = "hello world!";
+		test.deepEqual(result, expected);
+		test.done();
+	},
+	
+	testStringWithMultipleTags:function(test) {
+		var testString = "hello <a>world!</a>";
+		var result = sanitizeInputs.removeTags(testString);
+		var expected = "hello world!";
+		test.deepEqual(result, expected);
+		test.done();
+	},
+	
+	testString_TagsWithSpecialCharacters:function(test) {
+		var testString = '<a href="/index.html">hello</a> world!';
+		var result = sanitizeInputs.removeTags(testString);
+		var expected = "hello world!";
+		test.deepEqual(result, expected);
+		test.done();
+	}
+});
